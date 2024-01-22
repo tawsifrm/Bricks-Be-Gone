09ec1242267519c6da1fe9b020282a55b9ee2d38
Tawsif Mayaz <tawsifrm@gmail.com>
2024-01-22 02:32:11 -0500

    Created Brick Breaker game in Android Studio

---
+++ app/src/main/java/com/example/bricksbegone/Brick.java
@@ -0,0 +1,23 @@
+package com.example.bricksbegone;
+
+public class Brick {
+    private boolean isVisible;
+    public int row, column, width, height;
+
+    public Brick(int row, int column, int width, int height) {
+        isVisible = true;
+        this.row = row;
+        this.column = column;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setInvisible() {
+        isVisible = false;
+    }
+
+    public boolean getVisibility() {
+        return isVisible;
+    }
+
+}
